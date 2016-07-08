52d71dc83cee8bef2e93f9bd0e7591c8ad72799e
Jon Baclig <jon@baclig.com>
2016-07-08 11:01:14 -0700

    commit to revert

--- voting-client/src/index.jsx
+++ voting-client/src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+<<<<<<< HEAD
 import {Router, Route, hashHistory} from 'react-router';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
@@ -34,5 +35,13 @@ ReactDOM.render(
   <Provider store={store}>
     <Router history={hashHistory}>{routes}</Router>
   </Provider>,
+=======
+import Voting from './components/Voting';
+
+const pair = ['Trainspotting', '28 Days Later'];
+
+ReactDOM.render(
+  <Voting pair={pair} winner="Trainspotting"/>,
+>>>>>>> parent of 6936638... implement Results page
   document.getElementById('app')
 );
